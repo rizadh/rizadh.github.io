678d36486ba219dfe916e82c3d3f12208cbf86ea
Rizadh Nizam <rizadh@icloud.com>
2015-11-15 20:03:42 -0500

    Added Time Display
    Dial now has indicates percentage of completion in the middle.

--- js/timer.js
+++ js/timer.js
@@ -5,7 +5,8 @@ $(function() {
         }, {
             duration: 5000,
             progress: function(elements, complete, remaining, start, tweenValue) {
-                setDial($("svg path"), 50, tweenValue);
+                setDial($("#dial-ring path"), 50, tweenValue);
+                $("#dial-text").html(Math.round(tweenValue*100)+"%");
             },
             loop: true,
             easing: "easeOut"
@@ -15,12 +16,15 @@ $(function() {
 
 function setDial(dial, radius, progress) {
     /*
-    Set given SVG path element (dial) to be a partial circle with a given radius. The progress parameter represents the partiality of the circle, 1 creating a complete circle, 0.5 creating a semi-circle and 0 creating no circle
+    Set given SVG path element (dial) to be a partial circle with a given
+    radius. The progress parameter represents the partiality of the circle,
+    1 creating a complete circle, 0.5 creating a semi-circle and 0 creating no
+    circle. Assumes that the circle assumes full width of the viewbox.
     */
     var sweep = progress >= 0.5 ? 1 : 0;
     dial.attr({
         d: "M" + radius + " " + radius + "L" + radius + " 0A" + radius + " " + radius + " 0 " + sweep + " 1 " + (radius + radius*Math.sin(2*Math.PI*progress)) + " " + (radius - radius*Math.cos(2*Math.PI*progress)),
         fill: "hsla(" + (120 * (1 - progress)) + ", 100%, 50%, 1)"
     });
-    dial.parent().children("circle").attr("fill", "hsla(" + (120 * (1 - progress)) + ", 100%, 50%, 0.1)");
+    dial.siblings("circle").attr("fill", "hsla(" + (120 * (1 - progress)) + ", 100%, 50%, 0.1)");
 }
