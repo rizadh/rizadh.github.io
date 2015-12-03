ba92f262d00330604a47b950e21a5e4545113aad
Rizadh Nizam <rizadh@icloud.com>
2015-12-02 21:00:14 -0500

    Revamp Timer Interface
    Timer now uses custom input menu and theme colours.

--- js/timer.js
+++ js/timer.js
@@ -2,107 +2,62 @@
 var WHOLE_CIRCLE = 0.99999;
 
 $(function() {
-    // Automatically begin demo mode when page is loaded
-    setTime(prompt("Enter time you want to countdown to (in seconds):"));
-
-    $("#dial").velocity({
-        rotateX: 0.8
-    }, 500);
-});
-
-// Set size of text to maximize use of dial area when page is loaded and when viewport changes
-$(window).on('load resize orientationChange', function(event) {
-    var max_length = Math.min(window.innerHeight, window.innerWidth);
-    $("#dial-text").css("font-size",max_length/5);
-});
-
-/** Sets dial of given radius to given progress */
-function setDial(dial, radius, progress) {
-    var sweep = progress >= 0.5 ? 1 : 0;
-    dial.attr({
-        d: "M" + radius + " " + radius + "L" + radius + " 0A" + radius + " " + radius + " 0 " + sweep + " 1 " + (radius + radius*Math.sin(2*Math.PI*progress)) + " " + (radius - radius*Math.cos(2*Math.PI*progress)),
-        fill: "hsla(" + (120 * (1 - progress)) + ", 100%, 50%, 1)"
+    setDisplayTime("");
+    $("#input-keypad td").click(function() {
+        key_value = $(this).text();
+        if (key_value == "Clear") {
+            setDisplayTime("");
+        } else if (key_value == "Start") {
+            startTimer();
+        } else {
+            var new_text = (getDisplayTime() + key_value).slice(-6);
+            setDisplayTime(new_text);
+        }
     });
-    dial.data("complete", progress);
-    dial.siblings("circle").attr("fill", "hsla(" + (120 * (1 - progress)) + ", 100%, 50%, 0.1)");
-}
+    $("#edit-button").click(function() {
+        editTime();
+    });
+});
 
-/** Change text of dial smoothly */
-function changeDialText(new_text) {
-    var current_text = $("#dial-text").text();
-    var diff_chars = differentChars(new_text, current_text);
+$(window).on('load resize orientationChange', function() {
+    // Maximize size of text
+    var max_length = Math.min(window.innerHeight, window.innerWidth);
+    $("body").css("font-size", max_length / 8);
+});
 
-    if (new_text.length !== current_text.length) {
-        var fill_array = [];
-        for (var i = 0; i < (Math.abs(current_text.length - new_text.length)); i++) {
-            fill_array[i] = Math.min(new_text.length,current_text.length) + i;
-        }
-        diff_chars = diff_chars.concat(fill_array);
-    }
+/** Set dial of given radius to given progress */
+function setDial(dial, radius, progress) {
+    // Calculate path parameters
+    var sweep = progress >= 0.5 ? 1 : 0;
+    var arc_pos_x = radius*(1 + Math.sin(2*Math.PI*progress));
+    var arc_pos_y = radius*(1 - Math.cos(2*Math.PI*progress));
+    var arc_pos = arc_pos_x + " " + arc_pos_y;
+    var arc_dimensions = radius + " " + radius;
+    var arc_parameters = " 0 " + sweep + " 1 ";
 
-    $("#dial-text.old").remove();
-    $("#dial-text")
-        .clone()
-        .addClass("old")
-        .appendTo($("#dial"))
-        .html(spanAtIndexes(current_text, diff_chars));
-    $("#dial-text")
-        .data("text", new_text)
-        .html(spanAtIndexes(new_text, diff_chars))
-        .children("span")
-        .css("opacity",0)
-        .velocity({
-            tween: 1
-        }, {
-            duration: 200,
-            progress: function(e, c, r, s, t) {
-                $("#dial-text:not(.old) span").css("opacity", t);
-                $("#dial-text.old span").css("opacity",1-t);
-            },
-            complete: function() {
-                $("#dial-text.old").remove();
-            },
-            easing: "linear"
-        });
-}
+    // Calculate path segments
+    var move_to_center = "M" + radius + " " + radius;
+    var move_to_start = "L" + radius + " 0";
+    var make_arc = "A" + arc_dimensions + arc_parameters + arc_pos;
 
-/** Given some text, wrap characters at the given indexes with span tags */
-function spanAtIndexes(text, indexes) {
-    var new_text = text.split("");
-    for (var i = 0; i < indexes.length; i++) {
-        if (indexes[i] < new_text.length) {
-            new_text[indexes[i]] = "<span class='same-text'>" + text[indexes[i]] + "</span>"
-        }
-    }
-    return new_text
-}
+    // Creath path from segments
+    var dial_path = move_to_center + move_to_start + make_arc;
 
-/** Find different characters between two string and return their indexes */
-function differentChars(string1, string2) {
-    var different_character_index_list = [];
-    for (i = 0; i < Math.min(string1.length,string2.length); i++) {
-        if (string1[i] !== string2[i]) {
-            different_character_index_list.push(i);
-        }
-    }
-
-    return different_character_index_list;
+    // Set path
+    dial.attr("d", dial_path)
 }
 
 function setTime(sec) {
-    // More than 36000 seconds equates to 10 hours+, which does not fit in the dial
-    if (sec >= 36000) {
-        setTime(prompt("The time you entered was too high. Enter a time less than 36000 seconds"));
-        return;
-    }
+    console.log("starting time");
     // Convert seconds to milliseconds
     dial_time = sec*1000;
-    $("#dial").velocity(
+    $("#dial-ring").velocity(
         {
             // Can't go to 1 with SVG arc
             tween: WHOLE_CIRCLE
         }, {
             duration: dial_time,
+            easing: "linear",
             progress: function(elements, complete, remaining, start, tweenValue) {
                 setDial($("#dial-ring path"), 50, tweenValue);
                 // Find seconds rounded up
@@ -111,25 +66,105 @@ function setTime(sec) {
                 var minutes = Math.floor(seconds/60) % 60;
                 // Find hours rounded down
                 var hours = Math.floor(seconds/3600);
+                // Find remaining seconds
                 seconds = seconds % 60;
-                // Force 2 digits for seconds if minutes are present
-                if (minutes > 0) {
-                    seconds = ("0"+ seconds).slice(-2);
-                }
-                // Force 2 digits for minutes if hours are present
-                if (hours > 0) {
-                    minutes = ("0"+ minutes).slice(-2);
-                }
-                // Create text to be inserted into dial
-                var dial_text = (hours > 0 ? hours + ":" : "") + (hours + minutes  > 0 ? minutes + ":" : "") + seconds;
-                if ($("#dial-text").data("text") != dial_text) {
-                    changeDialText(dial_text);
-                }
-            },
-            easing: "linear",
-            complete: function() {
-                changeDialText("0");
+                // Force 2 digits
+                seconds = ("0" + seconds).slice(-2);
+                minutes = ("0" + minutes).slice(-2);
+                hours = ("0" + hours).slice(-2);
+                // Send time text to display
+                setDisplayTime([hours, minutes, seconds].join(""));
             }
         }
     );
+}
+
+function getDisplayTime(actual) {
+    var display = $("#display-text");
+    return actual ? display.text() : display.data("current_time");
+}
+
+function setDisplayTime(text, actual) {
+    var display = $("#display-text");
+    if (actual) {
+        display.text(text);
+    } else if (text == "") {
+        display.data("current_time", "000000");
+        display.text("Enter a time");
+    } else {
+        display.data("current_time", ("000000" + text).slice(-6));
+        var current_time = getDisplayTime();
+        var time_array = [];
+        var unit_array = ["h","m","s"];
+        for (var i = 0; i < 3; i++) {
+            var time_value = current_time.slice(2*i,2*i+2);
+            if (time_value > 0 || time_array[i - 1] || unit_array[i] == "s") {
+                if (time_value < 10 && !time_array[0]) {
+                    time_value = time_value.slice(-1);
+                }
+                time_array.push(time_value + unit_array[i]);
+            }
+        }
+        var new_time = time_array.join(" ");
+        if (new_time != display.text()) {
+            display.text(new_time);
+        }
+    }
+}
+
+function startTimer() {
+    $("#input-display").velocity({
+        height: "90%"
+    }, {
+        duration: 400,
+        easing: "easeOutExpo",
+        begin: function() {
+            $("#input-keypad td").velocity("fadeOut", 200);
+        },
+        complete: function() {
+            $("#edit-button").velocity("fadeIn", 100);
+            $("#dial-ring").velocity({
+                opacity: [1, 0],
+                scale: [1, 0]
+            }, {
+                easing: "easeOutExpo",
+                display: "block",
+                duration: 400,
+                queue: false
+            });
+        }
+    });
+
+    var time_string = getDisplayTime();
+    var hours_to_seconds = time_string.slice(-6,-4)*3600;
+    var minutes_to_seconds = time_string.slice(-4,-2)*60;
+    var seconds = time_string.slice(-2)*1;
+    var total_seconds = hours_to_seconds + minutes_to_seconds + seconds;
+    setTime(total_seconds);
+}
+
+function editTime() {
+    $("#input-display").velocity({
+        height: "20%"
+    }, {
+        duration: 400,
+        easing: "easeOutExpo",
+        complete: function() {
+            $("#input-keypad td").velocity("fadeIn", {
+                duration: 200,
+                display: ""
+            });
+        },
+        begin: function() {
+            $("#edit-button").velocity("fadeOut", 100);
+            $("#dial-ring").velocity("stop").velocity({
+                opacity: 0,
+                scale: 0
+            }, {
+                easing: "easeOutExpo",
+                display: "none",
+                duration: 400
+            });
+        }
+    });
 }
