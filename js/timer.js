16576a46a2ccc89be79d1b7b46c365817ac1bda4
Rizadh Nizam <rizadh@icloud.com>
2015-12-22 01:08:10 -0500

    Timer ~ De-Nest Large Functions
    Nesting large functions within other functions leads to increased
    memory usage.

--- js/timer.js
+++ js/timer.js
@@ -239,58 +239,58 @@ function startupAnimation() {
             display: null
         });
     }
+}
 
-    /** Asks user if they want to restore the previous state of the app */
-    function askForRestore() {
-        var timeStarted = parseInt(localStorage.getItem('timeStarted'));
-        var durationSet = parseInt(localStorage.getItem('durationSet'));
-        var noButton = {
-            text: 'No',
-            style: 'alert',
-            clickFunction: function() {
-                localStorage.setItem('timeStarted', '0');
-                localStorage.setItem('durationSet', '0');
-            }
-        };
-        var sureButton = {
-            text: 'Sure',
-            style: 'emphasize',
-            clickFunction: function(e) {
-                var progress = 1 - timeLeft()/durationSet;
-                if (timeLeft() > 0) {
-                    $('#dial-ring path')
-                        .data('timeLeft', timeLeft());
-                    $('#dial-ring path')
-                        .data('progress', progress);
-                    startTimer(false, true);
-                } else {
-                    e.stopPropagation();
-                    showNotification(
-                        'Too late, the timer has already ended',
-                        [
-                            {
-                                text: 'Dismiss',
-                                style: 'normal',
-                                clickFunction: function() {}
-                            }
-                        ]
-                    );
-                }
+/** Asks user if they want to restore the previous state of the app */
+function askForRestore() {
+    var timeStarted = parseInt(localStorage.getItem('timeStarted'));
+    var durationSet = parseInt(localStorage.getItem('durationSet'));
+    var noButton = {
+        text: 'No',
+        style: 'alert',
+        clickFunction: function() {
+            localStorage.setItem('timeStarted', '0');
+            localStorage.setItem('durationSet', '0');
+        }
+    };
+    var sureButton = {
+        text: 'Sure',
+        style: 'emphasize',
+        clickFunction: function(e) {
+            var progress = 1 - timeLeft()/durationSet;
+            if (timeLeft() > 0) {
+                $('#dial-ring path')
+                    .data('timeLeft', timeLeft());
+                $('#dial-ring path')
+                    .data('progress', progress);
+                startTimer(false, true);
+            } else {
+                e.stopPropagation();
+                showNotification(
+                    'Too late, the timer has already ended',
+                    [
+                        {
+                            text: 'Dismiss',
+                            style: 'normal',
+                            clickFunction: function() {}
+                        }
+                    ]
+                );
             }
         }
-        if (timeStarted && durationSet) {
-            var timeThresholdSec = 10;
-            if (timeLeft() > timeThresholdSec*1000) {
-                var message = 'Seems like the timer was still running when' +
-                ' you last closed this app. Do you want to restore the timer?';
-                showNotification(message, [noButton, sureButton]);
-            }
+    }
+    if (timeStarted && durationSet) {
+        var timeThresholdSec = 10;
+        if (timeLeft() > timeThresholdSec*1000) {
+            var message = 'Seems like the timer was still running when' +
+            ' you last closed this app. Do you want to restore the timer?';
+            showNotification(message, [noButton, sureButton]);
         }
+    }
 
-        /** Returns time left until previous timer ends */
-        function timeLeft() {
-            return (timeStarted + durationSet) - (new Date()).getTime();
-        }
+    /** Returns time left until previous timer ends */
+    function timeLeft() {
+        return (timeStarted + durationSet) - (new Date()).getTime();
     }
 }
 
@@ -371,79 +371,79 @@ function setDisplayTime(text, actual, fancy) {
             changeDisplayText(newDisplayText, fancy ? 'fancy' : '');
         }
     }
+}
 
-    /**
-     * Changes the text displayed
-     * @param (jQuery) newDisplayText - The new #display element
-     * @param {string} style - The style of animation to use when changing text
-     */
-    function changeDisplayText(newDisplayText, style) {
-        $('#display-text-old').remove();
-        var displayText = $('#display-text');
-        if (style === 'fancy') {
-            var display_html = displayText.text();
-            var newDisplay_html = newDisplayText.text();
-            var display_htmlArray = [];
-            var newDisplay_htmlArray = [];
-            if (display_html.length === newDisplay_html.length) {
-                for (var i = 0; i < display_html.length; i++) {
-                    if (display_html[i] === newDisplay_html[i]) {
-                        display_htmlArray.push(display_html[i]);
-                        newDisplay_htmlArray.push(newDisplay_html[i]);
-                    } else {
-                        display_htmlArray.push('<span>');
-                        display_htmlArray.push(display_html[i]);
-                        display_htmlArray.push('</span>');
-                        newDisplay_htmlArray.push('<span>');
-                        newDisplay_htmlArray.push(newDisplay_html[i]);
-                        newDisplay_htmlArray.push('</span>');
-                    }
+/**
+ * Changes the text displayed
+ * @param (jQuery) newDisplayText - The new #display element
+ * @param {string} style - The style of animation to use when changing text
+ */
+function changeDisplayText(newDisplayText, style) {
+    $('#display-text-old').remove();
+    var displayText = $('#display-text');
+    if (style === 'fancy') {
+        var display_html = displayText.text();
+        var newDisplay_html = newDisplayText.text();
+        var display_htmlArray = [];
+        var newDisplay_htmlArray = [];
+        if (display_html.length === newDisplay_html.length) {
+            for (var i = 0; i < display_html.length; i++) {
+                if (display_html[i] === newDisplay_html[i]) {
+                    display_htmlArray.push(display_html[i]);
+                    newDisplay_htmlArray.push(newDisplay_html[i]);
+                } else {
+                    display_htmlArray.push('<span>');
+                    display_htmlArray.push(display_html[i]);
+                    display_htmlArray.push('</span>');
+                    newDisplay_htmlArray.push('<span>');
+                    newDisplay_htmlArray.push(newDisplay_html[i]);
+                    newDisplay_htmlArray.push('</span>');
                 }
-            } else {
-                display_htmlArray.push('<span>');
-                display_htmlArray.push(display_html);
-                display_htmlArray.push('</span>');
-                newDisplay_htmlArray.push('<span>');
-                newDisplay_htmlArray.push(newDisplay_html);
-                newDisplay_htmlArray.push('</span>');
             }
-
-            var replaceSpan = /<\/span><span>/gi;
-            displayText
-                .attr('id', 'display-text-old')
-                .html(display_htmlArray.join('').replace(replaceSpan,''))
-                .children('span')
-                .velocity({
-                    opacity: 0,
-                    scale: 0.5
-                }, {
-                    easing: EASE_OUT,
-                    duration: ANIMATION_DURATION,
-                    queue: false,
-                    display: 'inline-block',
-                    complete: function() {
-                        $(this).parent().remove();
-                    }
-                });
-
-            newDisplayText
-                .html(newDisplay_htmlArray.join('').replace(replaceSpan,''))
-                .appendTo('#display')
-                .children('span')
-                .css('opacity', 0)
-                .velocity({
-                    opacity: [1, 0],
-                    scale: [1, 0.5]
-                }, {
-                    easing: EASE_OUT,
-                    duration: ANIMATION_DURATION,
-                    queue: false,
-                    display: 'inline-block'
-                });
         } else {
-            displayText.remove();
-            newDisplayText.appendTo('#display');
+            display_htmlArray.push('<span>');
+            display_htmlArray.push(display_html);
+            display_htmlArray.push('</span>');
+            newDisplay_htmlArray.push('<span>');
+            newDisplay_htmlArray.push(newDisplay_html);
+            newDisplay_htmlArray.push('</span>');
         }
+
+        var replaceSpan = /<\/span><span>/gi;
+        displayText
+            .attr('id', 'display-text-old')
+            .html(display_htmlArray.join('').replace(replaceSpan,''))
+            .children('span')
+            .velocity({
+                opacity: 0,
+                scale: 0.5
+            }, {
+                easing: EASE_OUT,
+                duration: ANIMATION_DURATION,
+                queue: false,
+                display: 'inline-block',
+                complete: function() {
+                    $(this).parent().remove();
+                }
+            });
+
+        newDisplayText
+            .html(newDisplay_htmlArray.join('').replace(replaceSpan,''))
+            .appendTo('#display')
+            .children('span')
+            .css('opacity', 0)
+            .velocity({
+                opacity: [1, 0],
+                scale: [1, 0.5]
+            }, {
+                easing: EASE_OUT,
+                duration: ANIMATION_DURATION,
+                queue: false,
+                display: 'inline-block'
+            });
+    } else {
+        displayText.remove();
+        newDisplayText.appendTo('#display');
     }
 }
 
@@ -522,85 +522,87 @@ function startTimer(resume, restore) {
         setDisplayTime('Time too high', true, true);
     }
 
-    /**
-     * Initiate the timer
-     * @param {number} sec - Number of seconds to time
-     * @param {boolean} resume - If true, timer will resume from stored state
-     */
-    function setTime(sec, resume) {
-        // Convert seconds to milliseconds
-        var dialPath = $('#dial-ring path');
-        var dialTime = resume ? dialPath.data('timeLeft') : sec*1000;
-        var initialProgress = resume ? dialPath.data('progress') : 0;
 
-        dialPath.velocity('stop').velocity(
-            {
-                // Can't go to 1 with SVG arc
-                tween: [WHOLECIRCLE, initialProgress]
-            }, {
-                duration: dialTime,
-                easing: 'linear',
-                begin: function() {
-                    localStorage.setItem('timeStarted', (new Date()).getTime());
-                    localStorage.setItem('durationSet', dialTime);
-                },
-                progress: function(e, c, r, s, t) {
-                    setDial(dialPath, 40, t);
-                    // Store progress in data
-                    dialPath.data('progress', t);
-                    dialPath.data('timeLeft', r);
-                    // Find seconds rounded up
-                    var totalSeconds = Math.ceil(r/1000);
-                    // Find minutes rounded down
-                    var minutes = Math.floor(totalSeconds/60) % 60;
-                    // Find hours rounded down
-                    var hours = Math.floor(totalSeconds/3600);
-                    // Find remaining seconds
-                    var seconds = totalSeconds % 60;
-                    // Force 2 digits
-                    seconds = ('0' + seconds).slice(-2);
-                    minutes = ('0' + minutes).slice(-2);
-                    hours = ('0' + hours).slice(-2);
-                    var times = [hours, minutes, seconds];
-                    if (r !== 0) {
-                        // Send time text to display
-                        setDisplayTime(times.join(''), false, true);
-                    }
-                },
-                complete: function() {
-                    setDisplayTime('Done', false, true);
-                    $('#display').removeClass('running');
-                    localStorage.setItem('timeStarted', 0);
-                    localStorage.setItem('durationSet', 0);
+}
+
+/**
+ * Initiate the timer
+ * @param {number} sec - Number of seconds to time
+ * @param {boolean} resume - If true, timer will resume from stored state
+ */
+function setTime(sec, resume) {
+    // Convert seconds to milliseconds
+    var dialPath = $('#dial-ring path');
+    var dialTime = resume ? dialPath.data('timeLeft') : sec*1000;
+    var initialProgress = resume ? dialPath.data('progress') : 0;
+
+    dialPath.velocity('stop').velocity(
+        {
+            // Can't go to 1 with SVG arc
+            tween: [WHOLECIRCLE, initialProgress]
+        }, {
+            duration: dialTime,
+            easing: 'linear',
+            begin: function() {
+                localStorage.setItem('timeStarted', (new Date()).getTime());
+                localStorage.setItem('durationSet', dialTime);
+            },
+            progress: function(e, c, r, s, t) {
+                setDial(dialPath, 40, t);
+                // Store progress in data
+                dialPath.data('progress', t);
+                dialPath.data('timeLeft', r);
+                // Find seconds rounded up
+                var totalSeconds = Math.ceil(r/1000);
+                // Find minutes rounded down
+                var minutes = Math.floor(totalSeconds/60) % 60;
+                // Find hours rounded down
+                var hours = Math.floor(totalSeconds/3600);
+                // Find remaining seconds
+                var seconds = totalSeconds % 60;
+                // Force 2 digits
+                seconds = ('0' + seconds).slice(-2);
+                minutes = ('0' + minutes).slice(-2);
+                hours = ('0' + hours).slice(-2);
+                var times = [hours, minutes, seconds];
+                if (r !== 0) {
+                    // Send time text to display
+                    setDisplayTime(times.join(''), false, true);
                 }
+            },
+            complete: function() {
+                setDisplayTime('Done', false, true);
+                $('#display').removeClass('running');
+                localStorage.setItem('timeStarted', 0);
+                localStorage.setItem('durationSet', 0);
             }
-        );
-
-        /** Set dial of given radius to given progress */
-        function setDial(dial, arcRadius, progress) {
-            var boxRadius = 50;
-
-            // Calculate path parameters
-            var offset = boxRadius - arcRadius;
-            var sweep = progress >= 0.5 ? 1 : 0;
-            var arcPosX = offset + arcRadius*(1 + Math.sin(2*Math.PI*progress));
-            var arcPosY = offset + arcRadius*(1 - Math.cos(2*Math.PI*progress));
-            var arcPos = arcPosX + ' ' + arcPosY;
-            var arcDimensions = arcRadius + ' ' + arcRadius;
-            var arcParameters = ' 0 ' + sweep + ' 1 ';
-
-            // Calculate path segments
-            var moveToCenter = 'M' + boxRadius + ' ' + boxRadius;
-            var moveToStart = 'm0 ' + (-arcRadius);
-            var makeArc = 'A' + arcDimensions + arcParameters + arcPos;
-
-            // Creath path from segments
-            var dialPath = moveToCenter + moveToStart + makeArc;
-
-            // Set path
-            dial.attr('d', dialPath);
         }
-    }
+    );
+}
+
+/** Set dial of given radius to given progress */
+function setDial(dial, arcRadius, progress) {
+    var boxRadius = 50;
+
+    // Calculate path parameters
+    var offset = boxRadius - arcRadius;
+    var sweep = progress >= 0.5 ? 1 : 0;
+    var arcPosX = offset + arcRadius*(1 + Math.sin(2*Math.PI*progress));
+    var arcPosY = offset + arcRadius*(1 - Math.cos(2*Math.PI*progress));
+    var arcPos = arcPosX + ' ' + arcPosY;
+    var arcDimensions = arcRadius + ' ' + arcRadius;
+    var arcParameters = ' 0 ' + sweep + ' 1 ';
+
+    // Calculate path segments
+    var moveToCenter = 'M' + boxRadius + ' ' + boxRadius;
+    var moveToStart = 'm0 ' + (-arcRadius);
+    var makeArc = 'A' + arcDimensions + arcParameters + arcPos;
+
+    // Creath path from segments
+    var dialPath = moveToCenter + moveToStart + makeArc;
+
+    // Set path
+    dial.attr('d', dialPath);
 }
 
 /** Activates input mode */
