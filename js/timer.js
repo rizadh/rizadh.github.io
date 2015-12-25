288c9c5ed09022aca43be2f8d41b55ecc7973a90
Rizadh Nizam <rizadh@icloud.com>
2015-12-25 00:05:17 -0500

    Timer ~ Wrap JS to 80 characters
    Change indentation and line breaks to assure that line length does not
    exceed 80 characters.

--- js/timer.js
+++ js/timer.js
@@ -53,7 +53,8 @@ $(function () {
 	});
 	$('#edit-button').click(editTime);
 	$('#display-text').click(togglePause);
-	$('#notification-banner').on('click', '.button, .justmessage', hideNotification);
+	$('#notification-banner')
+		.on('click', '.button, .justmessage', hideNotification);
 
 	// Enable keyboard input
 	$(document).on('keydown', function (e) {
@@ -763,23 +764,22 @@ function hideNotification() {
 function hoverTouchUnstick() {
 	// Check if the device supports touch events
 	if ('ontouchstart' in document.documentElement) {
-		// Loop through each stylesheet
-		for (var sheetI = document.styleSheets.length - 1; sheetI >= 0; sheetI--) {
-			var sheet = document.styleSheets[sheetI];
-			// Verify if cssRules exists in sheet
-			if (sheet.cssRules) {
-				// Loop through each rule in sheet
-				for (var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
-					var rule = sheet.cssRules[ruleI];
-					// Verify rule has selector text
-					if (rule.selectorText) {
-						// Replace hover psuedo-class with active psuedo-class
-						rule.selectorText = rule.selectorText
-							.replace(':hover', ':active');
-					}
-				}
-			}
-		}
+	// Loop through each stylesheet
+	for (var sheetI = document.styleSheets.length - 1; sheetI >= 0; sheetI--) {
+	var sheet = document.styleSheets[sheetI];
+	// Verify if cssRules exists in sheet
+	if (sheet.cssRules) {
+	// Loop through each rule in sheet
+	for (var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
+	var rule = sheet.cssRules[ruleI];
+	// Verify rule has selector text
+	if (rule.selectorText) {
+	// Replace hover psuedo-class with active psuedo-class
+	rule.selectorText = rule.selectorText.replace(':hover', ':active');
+	}
+	}
+	}
+	}
 	}
 }
 
