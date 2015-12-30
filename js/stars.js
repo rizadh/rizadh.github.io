9783cc7594acf74c2d671923e755dcbfdce948e3
Rizadh Nizam <rizadh@icloud.com>
2015-12-30 13:45:22 -0500

    Stars ~ Fix bug that allowed some unauthorized stars to be respawned
    When changing the number of stars, some stars did not remove themselves
    from the DOM.

--- js/stars.js
+++ js/stars.js
@@ -1,22 +1,27 @@
 var stars = 100;
+var round_num = 0;
+
 $(function() {
-    spawnStars(stars);
+    spawnStars(stars, 0);
     $(document).click(changeNumberOfStars);
 });
 
 function changeNumberOfStars() {
+    $('svg').not(':nth-child(1)').remove();
     var prompt_text = 'There are currently ' + stars + ' stars. Enter a new number of stars below:';
     spawnStars(stars = prompt(prompt_text));
 }
 
 function spawnStars(num_stars) {
     $('svg').not(':nth-child(1)').remove();
+    round_num++
     for (var i = num_stars; i > 0; i--) {
-        spawnStar(true);
+        spawnStar(true, round_num);
     }
 }
 
-function spawnStar(delay) {
+function spawnStar(delay, round) {
+    if (round !== round_num) return false;
     var element = $('svg:nth-child(1)').clone().appendTo('body');
     var size = 5 + Math.random()*5;
     var x_position = Math.random()*100;
@@ -25,7 +30,7 @@ function spawnStar(delay) {
     var rotation_angle = -5 + Math.random()*10;
     var rotation_intensity = -45 + Math.random()*90;
     var color = 240 + Math.random()*120;
-    var startup_delay = delay ? Math.random()*stars*10 : 0;
+    var startup_delay = delay ? Math.random()*stars*10 : Math.random()*stars;
     var twinkle_intensity = 0 + Math.random()*0.5
     var start_brightness = 80 + Math.random()*20;
     var hook = $.Velocity.hook;
@@ -57,7 +62,7 @@ function spawnStar(delay) {
             duration: twinkle_duration,
             complete: function() {
                 $(this).remove();
-                spawnStar(false);
+                spawnStar(false, round);
             }
         });
 }
