8700de2419803f75143bd84af22d2a2b3df91ae9
Rizadh Nizam <rizadh@icloud.com>
2015-12-30 14:12:53 -0500

    Stars ~ Added code comments
    Minor code cleanup as well.

--- js/stars.js
+++ js/stars.js
@@ -1,28 +1,41 @@
+// Represents how many stars are to be display on screen
 var stars = 100;
+// Represents the round number (increments every time # of stars is changed)
 var round_num = 0;
 
 $(function() {
-    spawnStars(stars, 0);
+    // Spawn initial stars
+    spawnStars(stars);
+    // Clicking on document opens up prompt to change number of stars
     $(document).click(changeNumberOfStars);
 });
 
 function changeNumberOfStars() {
-    $('svg').not(':nth-child(1)').remove();
+    // Text to display to user
     var prompt_text = 'There are currently ' + stars + ' stars. Enter a new number of stars below:';
+    // Change number of stars to given input and spawn
     spawnStars(stars = prompt(prompt_text));
 }
 
 function spawnStars(num_stars) {
+    // Remove all previous stars
     $('svg').not(':nth-child(1)').remove();
+    // Increment round number
     round_num++
     for (var i = num_stars; i > 0; i--) {
+        // Spawn given number of stars in current round
         spawnStar(true, round_num);
     }
 }
 
 function spawnStar(delay, round) {
+    // Only spawn if star is in current round
     if (round !== round_num) return false;
+
+    // Clone master star
     var element = $('svg:nth-child(1)').clone().appendTo('body');
+
+    // Create randomized values for stars properties
     var size = 5 + Math.random()*5;
     var x_position = Math.random()*100;
     var y_position = Math.random()*100;
@@ -33,8 +46,12 @@ function spawnStar(delay, round) {
     var startup_delay = delay ? Math.random()*stars*10 : Math.random()*stars;
     var twinkle_intensity = Math.random()*1;
     var start_brightness = 80 + Math.random()*20;
+
+
+    // Create shortcut to hook
     var hook = $.Velocity.hook;
 
+    // Set constant properties of star
     element.css({
         display: 'block',
         width: size,
@@ -44,16 +61,22 @@ function spawnStar(delay, round) {
         fill: 'hsla(' + color + ', 100%, ' + start_brightness + '%, 1)'
     });
 
-    hook(element, 'rotateZ', (rotation_angle - rotation_intensity)+'deg');
+    // Hook variable properties of star
+    hook(element, 'rotateZ', (rotation_angle - rotation_intensity) + 'deg');
     hook(element, 'scale', '0');
     hook(element, 'opacity', '0');
+
+    // Animate variable properties of star
     element
+        // Add random delay to stagger fade in of stars
         .delay(startup_delay)
+        // Fade in star to full size
         .velocity({
             scale: 1,
             opacity: 1,
             rotateZ: rotation_angle
         }, twinkle_duration)
+        // Fade out star
         .velocity({
             scale: twinkle_intensity,
             opacity: 0,
@@ -61,7 +84,9 @@ function spawnStar(delay, round) {
         }, {
             duration: twinkle_duration,
             complete: function() {
+                // Remove star after one cycle
                 $(this).remove();
+                // Respawn star in the current round
                 spawnStar(false, round);
             }
         });
