0c723df147f5c87c8ed15c05589a139235bce8a8
Rizadh Nizam <rizadh@icloud.com>
2016-03-05 03:21:37 -0500

    Ripple ~ Update commenting

--- js/ripple.js
+++ js/ripple.js
@@ -1,17 +1,24 @@
 ripple = (function(){
     'use strict';
 
+    // Store ripple attributes
     var rippleSize, rippleColor, rippleDuration;
 
+    // Enable ripple effect
     function enable(size, color, duration) {
+        // Set ripple attributes
         rippleSize = size;
         rippleColor = color;
         rippleDuration = duration;
+        // Attach handler to click event
         $(document).on('click.ripple', function(event) {
+            // Create ripple div
             var $ripple = $('<div class="ripple"></div>');
+            // Hook initial properties
             $.Velocity.hook($ripple, 'translateX', '-50%');
             $.Velocity.hook($ripple, 'translateY', '-50%');
             $.Velocity.hook($ripple, 'scale', '0');
+            // Set styling, including position and size
             $ripple.css({
                     position: 'absolute',
                     top: event.pageY,
@@ -24,30 +31,38 @@ ripple = (function(){
                     borderRadius: '100%',
                     pointerEvents: 'none'
                 })
+                // Animate to full size and zero opacity
                 .velocity({
                     scale: 1,
                     opacity: 0
                 }, {
                     duration: rippleDuration,
                     easing: 'easeOutExpo',
-                    complete: function() { $(this).remove(); }
+                    // Remove element after completion of animation
+                    complete: function() {
+                        $(this).remove();
+                    }
                 })
                 .appendTo('body');
         });
     }
 
+    // Disable ripple effect
     function disable() {
         $(document).off('click.ripple');
     }
 
+    // Change size of ripple (with units)
     function changeSize(newSize) {
         rippleSize = newSize;
     }
 
+    // Change color of ripple (any acceptable CSS color string)
     function changeColor(newColor) {
         rippleColor = newColor;
     }
 
+    // Change duration of ripple
     function changeDuration(newDuration) {
         rippleDuration = newDuration;
     }
