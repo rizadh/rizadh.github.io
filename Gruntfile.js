47281dc9729b60ff6f53ffccb3b3186427565e6d
Rizadh Nizam <rizadh@icloud.com>
2016-04-05 14:54:14 -0400

    Aesthetic changes to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,11 @@ module.exports = function(grunt) {
     grunt.initConfig(configs);
 
     grunt.registerTask('default', ['compile']);
+
     grunt.registerTask('full', ['update', 'compile', 'lint']);
-    grunt.registerTask('update', ['bower-update', 'bower_concat',
-                                  'copy:normalize']);
-    grunt.registerTask('compile', ['scss', 'js']);
-    grunt.registerTask('scss', ['sass:dist', 'postcss:dist']);
-    grunt.registerTask('js', ['uglify:dist']);
-    grunt.registerTask('lint', ['htmllint', 'jshint']);
+        grunt.registerTask('update', ['bower-update', 'bower_concat', 'copy:normalize']);
+        grunt.registerTask('compile', ['scss', 'js']);
+            grunt.registerTask('scss', ['sass:dist', 'postcss:dist']);
+            grunt.registerTask('js', ['uglify:dist']);
+        grunt.registerTask('lint', ['htmllint', 'jshint']);
 };
